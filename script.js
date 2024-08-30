9ab38ccbe1ae71bcde27d24141465186d5d615bb
jatcollab88 <jo.tay@unsw.edu.au>
2024-08-30 13:00:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-// Custom stylesheet
+// Links stylesheet
 let lnk = document.createElement('link');
 lnk.setAttribute('href', 'https://jatcollab88.github.io/test/style.css');
 lnk.setAttribute('rel', 'stylesheet');
 document.head.appendChild(lnk);
 
+// Links jquery
+let src = document.createElement('script');
+src.setAttribute('src', 'https://code.jquery.com/jquery-3.7.1.slim.min.js');
+src.setAttribute('integrity', 'sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=');
+src.setAttribute('crossorigin', 'anonymous');
+document.head.appendChild(src);
 
-// Accordion
-$(document).on('click', '.myContainer .myTrigger', function() {
-	var container = $(this).closest('.myContainer');
-	container.find('.myContent').stop().slideToggle(350);
-});
+// Checks jquery has loaded
+var jqReady = setInterval(function(){
+	if (typeof $ != 'undefined') {
+		clearInterval(jqReady);
+		runJqFuncs();
+	}
+}, 50);
+
+// Functions that are reliant on jquery
+function runJqFuncs() {
+	// Accordion
+	$(document).on('click', '.myContainer .myTrigger', function() {
+		var container = $(this).closest('.myContainer');
+		container.find('.myContent').stop().slideToggle(350);
+	});
+};
